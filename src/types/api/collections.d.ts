83ecf81736f2d9ee90ad5c617e0d0da98eddf19b
Flame <5017_30@mail.ru>
2024-10-08 17:12:46 +0300

    refactor: remove fields user

--- src/types/api/collections.d.ts
+++ src/types/api/collections.d.ts
@@ -9,28 +9,6 @@ declare namespace Collections {
         isAdmin: boolean
         password: string
         birthdate: string
-        shortInfo: string | null
-        city: string | null
-        mobilePhone: string | null
-        additionalPhone: string | null
-        skype: string | null
-        site: string | null
-        activity: string | null
-        interests: string | null
-        music: string | null
-        movies: string | null
-        TVShows: string | null
-        books: string | null
-        games: string | null
-        quotes: string | null
-        friends: number[]
-        incomingFriendRequests: number[]
-        outgoingFriendRequests: number[]
-        grandparents: string[]
-        parents: string[]
-        siblings: string[]
-        children: string[]
-        grandsons: string[]
         updatedAt: string
         createdAt: string
     }
