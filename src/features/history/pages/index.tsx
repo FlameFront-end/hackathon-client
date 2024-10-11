a48315d5c6eda0d1c18a3e5ae15234e6bb13a979
Flame <5017_30@mail.ru>
2024-10-11 05:09:46 +0300

    fix: history

--- src/features/history/pages/index.tsx
+++ src/features/history/pages/index.tsx
@@ -1,27 +1,51 @@
-import { type FC } from 'react'
+import { type FC, useState } from 'react'
 import { HistoryWrapper } from './History.styled.tsx'
-import { Header } from '@/features/kit'
-import { useGetAllHistoryQuery } from '../api/history.api.ts'
+import { Header, PrimaryButton } from '@/features/kit'
+import { useGetAllHistoryQuery, useGetHistoryByUserIdQuery } from '../api/history.api.ts'
 import List from '../components/List'
+import { useAppSelector } from '@/hooks'
 
 const History: FC = () => {
+    const userId = useAppSelector(state => state.auth.user.id)
+
     const { data: allHistory, isFetching } = useGetAllHistoryQuery(null)
+    const { data: myHistory, isFetching: isFetchingMyHistory } = useGetHistoryByUserIdQuery(userId ?? 0)
+
+    const [showAll, setShowAll] = useState<boolean>(true)
 
     return (
         <HistoryWrapper>
             <Header subheading='Геоданные и измерения' />
-            {allHistory && <div className='history'>
-                <List isFetching={isFetching} data={allHistory.map((item) => ({
-                    userName: item.user?.nick,
-                    location: 'название локации',
-                    time: item.createdAt,
-                    indicators: {
-                        downloadSpeed: item.downloadSpeed,
-                        uploadSpeed: item.uploadSpeed
-                    }
-                }))}/>
-            </div>}
+            {userId ? <PrimaryButton onClick={() => { setShowAll((prevState) => !prevState) }}>
+                {showAll ? 'Показать мою историю' : 'Показать историю всех пользователь'}
+            </PrimaryButton> : null}
 
+            {showAll ? <>
+                {allHistory && <div className='history'>
+                    <List isFetching={isFetching} data={allHistory.map((item) => ({
+                        userName: item.user?.nick,
+                        location: 'название локации',
+                        time: item.createdAt,
+                        indicators: {
+                            downloadSpeed: item.downloadSpeed,
+                            uploadSpeed: item.uploadSpeed
+                        }
+                    }))}/>
+                </div>}
+            </> : <>
+                {myHistory && <div className='history'>
+                    <List isFetching={isFetchingMyHistory} data={myHistory.map((item) => ({
+                        userName: item.user?.nick,
+                        location: 'название локации',
+                        time: item.createdAt,
+                        indicators: {
+                            downloadSpeed: item.downloadSpeed,
+                            uploadSpeed: item.uploadSpeed
+                        }
+                    }))}/>
+                </div>}
+            </>
+            }
         </HistoryWrapper>
     )
 }
